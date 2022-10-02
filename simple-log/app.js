3aadc0f4a27da1f9854c418d5d9a40efa01a1524
MartinSob <martin.sobrero.m@gmail.com>
2022-10-02 20:53:59 -0300

    Add presigned URL

--- simple-log/app.js
+++ simple-log/app.js
@@ -1,8 +1,28 @@
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 exports.handler = async (event, context) => {
+    const client = new S3Client({
+        // The region of the bucket
+        region: 'us-east-1',
+    });
+    const command = new PutObjectCommand({
+        // The name of the bucket
+        Bucket: process.env.BUCKET_NAME,
+        // The name with which the file will be uploaded
+        Key: 'file.txt'
+    });
+
+    const url = await getSignedUrl(client, command, { 
+        // Time in seconds before the presigned URL expires
+        expiresIn: 3600
+    });
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-            response: 'Success'
+            response: 'Success',
+            url: url
         })
     }
 }
